92ba7913ecd255bc06f313a0018974dfcb11c4aa
PolaKubiak <138911496+PolaKubiak@users.noreply.github.com>
2024-02-05 20:00:25 +0100

    Update zdalny task_2024.py

--- task_2024.py
+++ task_2024.py
@@ -1,11 +1,14 @@
 shopping = [("piekarnia", ["chleb", "bułki", "pączek"]), ("warzywniak", ["marchew", "seler", "rukola"])]
-
+#testowanie edycji kodu zdalnie aaaaaaa
 print("Lista zakupów")
 
 for shop, item in shopping:
     print(f"Idę do {shop.capitalize()} i kupuję tam {item} ")
-
+#kolejny testttttttttttttt
 shopping_new = dict(shopping)
 
 print(f"W sumie kupuję {len(shopping_new.get("piekarnia")) + len(shopping_new.get("warzywniak"))} produktów")
+print("Działa, czy nie działa?")
+
+
 
