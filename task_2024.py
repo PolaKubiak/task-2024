319e76d123969b15b7d959f829b77e12804f5fd1
PolaKubiak <138911496+PolaKubiak@users.noreply.github.com>
2024-02-05 21:08:12 +0100

    Update nr dwa do task_2024.py

--- task_2024.py
+++ task_2024.py
@@ -1,6 +1,6 @@
 shopping = [("piekarnia", ["chleb", "bułki", "pączek"]), ("warzywniak", ["marchew", "seler", "rukola"])]
 #testowanie edycji kodu zdalnie aaaaaaa
-print("Lista zakupów")
+#usunięte - poprawka nr 2
 
 for shop, item in shopping:
     print(f"Idę do {shop.capitalize()} i kupuję tam {item} ")
@@ -10,5 +10,5 @@ shopping_new = dict(shopping)
 print(f"W sumie kupuję {len(shopping_new.get("piekarnia")) + len(shopping_new.get("warzywniak"))} produktów")
 print("Działa, czy nie działa?")
 
-
+#czy działa? do commitu nr 2 
 
